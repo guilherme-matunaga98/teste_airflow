1d3b439ea71a442bc551c40fce0c53a0f4f57f8e
guilherme-matunaga98 <guilherme.matunaga@larroude.com>
2025-06-26 12:12:50 -0300

    fix1

--- dag_gui.py
+++ dag_gui.py
@@ -1,18 +1,19 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
 
-with DAG(
-    dag_id="example_gui",
-    start_date=datetime(2023, 10, 1),
-    schedule_interval="@daily",
-    default_args={
+default_args={
         "owner": "airflow",
         "retries": 1,
         "retry_delay": timedelta(minutes=5),
-    },
-    catchup=False,  # Evita execução retroativa desde 2023
-    tags=["example"]
+    }
+
+with DAG(
+    dag_id="example_gui",
+    default_args=default_args,
+    start_date=datetime(2023, 10, 1),
+    schedule_interval="@daily"
 ) as dag:
     task1 = BashOperator(
         task_id='first_task',
