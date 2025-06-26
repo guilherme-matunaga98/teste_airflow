ac244596367b6d9308192fb2a61c7d597ed48824
guilherme-matunaga98 <guilherme.matunaga@larroude.com>
2025-06-26 14:14:16 -0300

    task three

--- dag_gui.py
+++ dag_gui.py
@@ -10,7 +10,7 @@ default_args={
     }
 
 with DAG(
-    dag_id="example_gui_v2",
+    dag_id="example_gui_v3",
     default_args=default_args,
     start_date=datetime(2023, 10, 1),
     schedule_interval="@daily"
@@ -25,4 +25,10 @@ with DAG(
         bash_command='echo "This is the second task."'
     )
 
+    task3 = BashOperator(
+        task_id='third_task',
+        bash_command='echo "This is the third task."'
+    )
+
     task1.set_downstream(task2)
+    task1.set_downstram(task3)
